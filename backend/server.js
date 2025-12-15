cd8f08467a490706c20f6e78a3ee2f55596d314b
reginabeltran06 <reginabeltran06@gmail.com>
2025-12-15 13:49:47 -0600

    prueba

--- backend/server.js
+++ backend/server.js
@@ -21,14 +21,3 @@ app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 const path = require("path");
-
-// Solo en producción
-if (process.env.NODE_ENV === "production") {
-  // Servir los archivos estáticos del build de React
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  // Para cualquier ruta que no coincida con la API, devolver index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
-}
