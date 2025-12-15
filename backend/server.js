59fdacc2b468bf272769737792d9954843d4fba5
reginabeltran06 <reginabeltran06@gmail.com>
2025-12-15 14:27:56 -0600

    pruebaa

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
+
 
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import personsRoutes from "./routes/persons.routes.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// __dirname en ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
@@ -25,18 +19,5 @@ mongoose
 // Rutas API
 app.use("/api/persons", personsRoutes);
 
-// Servir frontend Vite en producción
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // Cambiado de "*" a "/*"
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
-
-// Puerto
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
