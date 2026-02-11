197490f46f51dd0825caa4975262ef0e2c399bd3
reginabeltran <reginabeltran06@gmail.com>
2026-02-10 22:55:31 -0600

    vercel

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,24 @@
-
-
+// server.js (o app.js)
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import personsRoutes from "./routes/persons.routes.js";
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conexión a MongoDB
-mongoose
-  .connect(process.env.MONGO_URI,{
-    serverSelectionTimeoutMS: 30000, // <--- Aumenta el tiempo de espera de selección del servidor a 30 segundos
+mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 30000,
     bufferTimeoutMS: 30000,
   })
-
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-// Rutas API
-app.use("/api/persons", personsRoutes);
+mongoose.connection.on("connected", () => {
+  console.log("DB:", mongoose.connection.name);
+});
 
+app.use("/api/persons", personsRoutes);
 
 export default app;
